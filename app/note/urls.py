d9f9d8b258f19f8f9ded8f9a72f347608e7aa42a
Ferdi Ramdhon <ferdirn@gmail.com>
2015-04-12 09:03:59 +0700

    add url detail

--- app/note/urls.py
+++ app/note/urls.py
@@ -5,4 +5,5 @@ from app.note import views
 urlpatterns = patterns('',
     # /note
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<id>\d+)/$', views.DetailView.as_view(), name='detail'),
 )
