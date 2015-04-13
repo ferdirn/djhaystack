6bdc459566a946b04db178b56198fa13e3f5770b
Ferdi Ramdhon <ferdirn@gmail.com>
2015-04-13 11:42:22 +0700

    revamp note/admin.py

--- app/note/admin.py
+++ app/note/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from app.note.models import Note
+
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'user')
+    # list_filter = ('pub_date',)
+    search_fields = ('title', 'body')
+    readonly_fields = ('user',)
+
+    fieldsets = [
+        ('', {'fields': ['title', 'body', 'user']}),
+        ('Date Information', {'fields': ['pub_date']})
+    ]
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.user = request.user
+        obj.save()
+
 
 # Register your models here.
+
+admin.site.register(Note, NoteAdmin)
