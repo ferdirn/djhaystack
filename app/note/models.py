7091caabc3f9018d924ce0d1180afde37df3424b
Ferdi Ramdhon <ferdirn@gmail.com>
2015-04-14 11:06:59 +0700

    update note/models

--- app/note/models.py
+++ app/note/models.py
@@ -1,12 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Note(models.Model):
-    user = models.ForeignKey(User)
-    pub_date = models.DateTimeField()
+    user = models.ForeignKey('auth.User')
+    pub_date = models.DateTimeField('date created', default=timezone.now)
     title = models.CharField(max_length=200)
     body = models.TextField()
+
+    class Meta:
+        get_latest_by = 'pub_date'
+        ordering = ('-pub_date',)
+        verbose_name_plural = 'notes'
 
     def __unicode__(self):
         return self.title
