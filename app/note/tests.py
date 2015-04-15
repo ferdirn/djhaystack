c22f502d7c648d9787177f7363da020f04f5422b
Ferdi Ramdhon <ferdirn@gmail.com>
2015-04-15 14:48:05 +0700

    add test create new note

--- app/note/tests.py
+++ app/note/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from model_mommy import mommy
+from app.note.models import Note
 
-# Create your tests here.
+
+
+class NoteTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(first_name='Ferdi', last_name='Ramdhon')
+
+    def test_create_new_note(self):
+        title = 'A new note'
+        note = mommy.make(Note, title=title, user=self.user)
+        self.assertEqual(note.__unicode__(), title)
