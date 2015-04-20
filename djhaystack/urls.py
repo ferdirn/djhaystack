33f0840fa84b1af8878b0811f0eb063b4810ab3d
Ferdi Ramdhon <ferdirn@gmail.com>
2015-04-20 10:40:26 +0700

    update djhaystack/urls.py

--- djhaystack/urls.py
+++ djhaystack/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'djhaystack.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', RedirectView.as_view(url='note/', permanent=False), name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^note/', include('app.note.urls', namespace='note')),
     url(r'^search/', include('haystack.urls')),
